2330e1ff36e8d10a831b1b210b0b30894bc8f203
hiteshchoudhary <hitesh@hiteshchoudhary.com>
2024-01-04 20:53:13 +0530

    Update page title and footer text

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -23,10 +23,10 @@ export default function Home() {
       <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
         <section className="text-center mb-8 md:mb-12">
           <h1 className="text-3xl md:text-5xl font-bold">
-            Dive into the World of Anonymous Conversations
+            Dive into the World of Anonymous Feedback
           </h1>
           <p className="mt-3 md:mt-4 text-base md:text-lg">
-            Explore Mystery Message - Where your identity remains a secret.
+            True Feedback - Where your identity remains a secret.
           </p>
         </section>
 
@@ -60,7 +60,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2023 Mystery Message. All rights reserved.
+        © 2023 True Feedback. All rights reserved.
       </footer>
     </>
   );
