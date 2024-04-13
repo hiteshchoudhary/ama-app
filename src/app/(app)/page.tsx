39a3b65959bfed16f627c39c044164da9b0715c9
Aditya raj <meadityaraj0001@gmail.com>
2024-04-13 12:53:18 +0530

    made the date dynamic

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -17,6 +17,8 @@ import {
 } from '@/components/ui/carousel';
 
 export default function Home() {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
   return (
     <>
       {/* Main content */}
@@ -60,7 +62,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2023 True Feedback. All rights reserved.
+        © {year} True Feedback. All rights reserved.
       </footer>
     </>
   );
