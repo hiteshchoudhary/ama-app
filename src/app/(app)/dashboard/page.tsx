b5a511db65be111c5b91227c9efe0a692f48a6be
Adarsh Sharma <124702646+adarshsharma7@users.noreply.github.com>
2024-06-11 20:57:38 +0530

    Fix incorrect toast message on sign-in or messages refresh when no messages are present in dashboard

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -71,7 +71,6 @@ function UserDashboard() {
       } catch (error) {
         const axiosError = error as AxiosError<ApiResponse>;
         toast({
-          title: 'Error',
           description:
             axiosError.response?.data.message ?? 'Failed to fetch messages',
           variant: 'destructive',
